ff6968a5427eae1c01adcd86009d282e8518c94a
shubh <shubh@LAPTOP-7UOGCAU7>
2025-10-18 12:43:38 +0530

    CHANGES

---
+++ src/main/java/com/example/url_shotern/UrlShoternApplication.java
@@ -0,0 +1,14 @@
+package com.example.url_shotern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShoternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShoternApplication.class, args);
+		System.out.println("Url Shotening");
+	}
+
+}
